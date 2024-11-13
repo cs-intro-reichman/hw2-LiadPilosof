bbabcf8ba2e038894591c42193739bb88e0f0806
Liad Pilosof <liad@Liads-MacBook-Air.local>
2024-11-13 11:41:21 +0200

    test

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,32 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+		int minToAdd = Integer.parseInt(args[1]);
+
+		int totalMin = Integer.parseInt(time.substring(0,1)) * 60 + Integer.parseInt(time.substring(2,3)) + minToAdd;
+		int totalHours = totalMin / 60;
+
+		int newHours = totalHours % 24;
+		int newMin = totalMin - (totalHours * 60);
+
+
+        System.out.println("gitn");
+		if (newHours<12) {
+			if (newMin<10) {
+				System.out.println("0"+newHours+":"+"0"+newMin);
+			}
+			else{
+				System.out.println("0"+newHours+":"+newMin);
+			}
+		}
+		else{
+			if (newMin<10) {
+				System.out.println(newHours+":"+"0"+newMin);
+			}
+			else{
+				System.out.println(newHours+":"+newMin);
+			}
+		}
+	
     }
 }
