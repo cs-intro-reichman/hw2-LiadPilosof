05ed1f84e0b0b86b94461875809f6e384181d172
LiadPilosof <liadbetzalel.pilosof@post.runi.ac.il>
2024-11-13 12:16:42 +0200

    test

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,16 +1,28 @@
 public class TimeCalc {
     public static void main(String[] args) {
+
         String time = args[0];
 		int minToAdd = Integer.parseInt(args[1]);
 
-		int totalMin = Integer.parseInt(time.substring(0,1)) * 60 + Integer.parseInt(time.substring(2,3)) + minToAdd;
+
+		String hoursTemp = "";
+		String minTemp = "";
+
+
+		for (int i = 0; i < time.indexOf(":"); i++) {
+			hoursTemp+=time.charAt(i);
+		}
+		for (int i = time.indexOf(":")+1; i < time.length(); i++) {
+			minTemp+=time.charAt(i);
+		}
+
+		int totalMin = Integer.parseInt(hoursTemp) * 60 + Integer.parseInt(minTemp) + minToAdd;
 		int totalHours = totalMin / 60;
 
 		int newHours = totalHours % 24;
 		int newMin = totalMin - (totalHours * 60);
 
 
-        System.out.println("gitn");
 		if (newHours<12) {
 			if (newMin<10) {
 				System.out.println("0"+newHours+":"+"0"+newMin);
